c7364c0ade2817f0f18cd6220f201e187f223f4a
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2016-12-08 00:14:33 +0530

    Adding comments to Team

--- Team.cpp
+++ Team.cpp
@@ -1,49 +1,43 @@
 #include "Team.h"
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include "Input.h"
+
+#include <iostream>	// For cout
+#include <fstream>	// For ifstream, ofstream
+#include <iomanip>	// For setw()
+
+#include "Input.h"	// For getString()
 
 using namespace std;
 
 
-
-Team::Team()
+Team::Team()	// Default Constructor
 	:name{ "INDIVIDUALS" }
 {
 }
-
-Team::Team(string name, vector<Player> newPlayers)
-	: name{ name }, players{ newPlayers }
-{
-	for (const Player& player : newPlayers) {
-		oversBowled.insert(pair<Player, int>(player, 0));
-	}
-}
-Team::Team(string name)
+Team::Team(string name)	// Initializes players to empty only
 	: name{ name }
 {
 }
-
-Team::~Team()
+Team::Team(string name, vector<Player> newPlayers)
+	: name{ name }, players{ newPlayers }
 {
+	for (const Player& player : newPlayers) {	// Set Overs Bowled of each Player to 0
+		oversBowled.insert(pair<Player, int>(player, 0));
+	}
 }
 
-
-bool Team::canBowl(const Player& player)
+Team::~Team()	// Destructor
 {
-	if (oversBowled[player] >= 4)
-		return false;
-	return true;
 }
 
 
 void Team::addPlayer(const Player& player)
 {
-	if (findPos(players, player) != -1)
+	// Assumes Player is not already present
+	if (findPos(players, player) != -1)	// Do nothing if player is already present
 		return;
+
 	players.push_back(player);
-	oversBowled.insert(pair<Player, int>(player, 0));
+	oversBowled.insert(pair<Player, int>(player, 0));	// Initialize Player's Overs Bowled to 0
 }
 
 Player* Team::getNextBatsman(vector<Player> & batsmenPlayed)
@@ -52,16 +46,18 @@ Player* Team::getNextBatsman(vector<Player> & batsmenPlayed)
 
 		displayBatsmen();
 
-		string batsman = getString("\nEnter the name of next batsman:\n");
+		string batsman = getString("\nEnter the name of next batsman:\n");	// Get name of next Batsman from User through cin
 
 		int posBatsman = findPos(players, Player(batsman));
+
 		if (posBatsman == -1) {
 			cout << " \nPlayer not found!!\n";
 		}
-		else if (findPos(out, players[posBatsman]) != -1) {
+		else if (findPos(out, players[posBatsman]) != -1) {	// If player is present in out
 			cout << '\n' << batsman << " already out!!\n";
 		}
 		else {
+			// If Player is Eligible to Play
 			batsmenPlayed.push_back(players[posBatsman]);
 			Player* ptrBatsman = &(players[posBatsman]);
 			return ptrBatsman;
@@ -77,15 +73,18 @@ Player* Team::getNextBowler()
 
 		displayBowlers();
 
-		string bowler = getString("\nEnter the name of next bowler:\n");
+		string bowler = getString("\nEnter the name of next bowler:\n");	// Get name of next Batsman from User through cin
 
 		int posBowler = findPos(players, Player(bowler));
+
 		if (posBowler == -1)
 			cout << "\nPlayer not found!!\n";
+
 		else if (!canBowl(players[posBowler])) {
 			cout << '\n' << bowler << " already bowled maximum overs bowled i.e. " << maxOvers << " overs!!\n";
 		}
 		else {
+			// If Player is Eligible to Bowl Next
 			oversBowled[players[posBowler]]++;
 			Player* ptrBowler = &(players[posBowler]);
 			return ptrBowler;
@@ -96,59 +95,37 @@ Player* Team::getNextBowler()
 
 }
 
-
-void Team::displayBatsmen()
+void Team::playerOut(const Player& player)
 {
-	cout << "\n**********  Available Batsmen  **********\n";
-	cout << endl << setw(25) << "Batsman Name" << setw(10) << "Skills" << "\n\n";
-	for (const Player& player : players) {
-		if (findPos(out, player) == -1) {
-			cout << setw(25) << player.name << setw(10) << player.getBattingSkills() << '\n';
-		}
-	}
-	cout << "#---------------------------------------#\n";
-	cout << endl;
+	// Assumes Player is not already out
+	if (findPos(out, player) != -1)	// Do nothing if Player is already Out
+		return;
+
+	out.push_back(player);
 }
-
-void Team::displayBowlers() 	// IN PROGRESS
+bool Team::canPlay(int nWickets, int nOvers) const
 {
-	system("cls");
-	cout << "***************  Available Bowlers  ***************\n";
-	cout << endl << setw(25) << "Bowler Name" << setw(10) << "Skills" << setw(15) << "Overs Bowled"<<"\n\n";
-	for (const Player& player : players) {
-		if (canBowl(player)) {
-			cout << setw(25) << player.name << setw(10) << player.getBowlingSkills() << setw(15) << oversBowled[player] << '\n';
-		}
-	}
-	cout << "#-------------------------------------------------#\n\n";
+	// Check if Player has already bowled max Overs allowed
+	return (players.size() >= nWickets && (players.size()*maxOvers) >= nOvers);
 }
 
 void Team::reset()
 {
 	out.clear();
 
-	for (const Player& player : players)
+	for (const Player& player : players)	// Reset Overs Bowled of all players back to 0
 		oversBowled[player] = 0;
-}
-
-void Team::playerOut(const Player& player)
-{
-	if (findPos(out, player) != -1)
-		return;
-	out.push_back(player);
-}
-
-bool Team::canPlay(int nWickets, int nOvers) const
-{
-	return (players.size() >= nWickets && (players.size()*maxOvers) >= nOvers);
 }
 
 void Team::updateBatsmen(const vector<Player> & batsmenPlayed)
 {
+	// Assumes that all player in batsmenPlayed are present in players
 	for (const Player& player : batsmenPlayed) {
+
 		int posBatsman = findPos(players, player);
-		if (posBatsman == -1)
+		if (posBatsman == -1)	// Do nothing if Player is not present in players
 			return;
+
 		players[posBatsman].increaseMatches(1);
 
 	}
@@ -160,14 +137,14 @@ void Team::updateBowlers()
 	}
 }
 
-
 void Team::write(ofstream& ofs) const
 {
 	if (!ofs)
 		throw runtime_error("Error writing to File");
 
+	// Write to the File in the specified format
 	ofs << name << '\n';
-	for (const Player& player : players) {
+	for (const Player& player : players) {	// Write each player in players
 		player.write(ofs);
 	}
 }
@@ -175,7 +152,9 @@ void Team::read(ifstream& ifs)
 {
 	if (!ifs)
 		throw runtime_error("Error reading from File");
+
 	vector<Player> temp;
+
 	getline(ifs, name);
 
 	while (true) {
@@ -185,11 +164,15 @@ void Team::read(ifstream& ifs)
 			Player player;
 			player.read(ifs);
 			temp.push_back(player);
+
+			// Check if the Player is followed by ')' or not
 			ifs >> ch;
-			if (ch != ')')
+			if (ch != ')')	// Wrong Format
 				throw runtime_error("Error reading from File");
 		}
 		else {
+			// New Team
+			// Place back the first Character of Next Team
 			ifs.unget();
 			break;
 		}
@@ -201,12 +184,53 @@ void Team::read(ifstream& ifs)
 
 
 
+void Team::displayBatsmen()
+{
+	cout << "\n**********  Available Batsmen  **********\n";
 
+	cout << endl << setw(25) << "Batsman Name" << setw(10) << "Skills" << "\n\n";
+
+	for (const Player& player : players) {	// For each Player in players
+		if (findPos(out, player) == -1) {	// If the player is not already out
+			// Display name and Batting Skill of the Player
+			cout << setw(25) << player.name << setw(10) << player.getBattingSkills() << '\n';
+		}
+	}
+
+	cout << "#---------------------------------------#\n\n";
+}
+void Team::displayBowlers()
+{
+	system("cls");
+	cout << "***************  Available Bowlers  ***************\n\n";
+
+	cout << setw(25) << "Bowler Name" << setw(10) << "Skills" << setw(15) << "Overs Bowled" << "\n\n";
+
+	for (const Player& player : players) {	// For each Player in players
+		if (canBowl(player)) {
+			cout << setw(25) << player.name << setw(10) << player.getBowlingSkills() << setw(15) << oversBowled[player] << '\n';
+		}
+	}
+	cout << "#-------------------------------------------------#\n\n";
+}
+
+bool Team::canBowl(const Player& player)
+{
+	if (oversBowled[player] >= maxOvers)	// If bowler already bowled max Overs
+		return false;
+
+	// If Player has not bowled all allowed Overs, he is eligible to bowl
+	return true;
+}
+
+
+
+// Operator Overloads
 bool operator==(const Team& team1, const Team& team2)
 {
-	return (team1.name == team2.name);
+	return (team1.name == team2.name);	// Equality based on name only
 }
 bool operator!=(const Team& team1, const Team& team2)
 {
-	return (team1.name != team2.name);
+	return (team1.name != team2.name);	// Inequality based on name only
 }
--- Team.h
+++ Team.h
@@ -1,63 +1,84 @@
 #pragma once
 
-#include <string>	// NOT REQUIRED
+#include <string>	// Just for completion of Documentation, Not required, already included in Player.h
 #include <vector>
 #include <unordered_map>
 
-
 #include "Player.h"
 
-const int maxOvers = 4;
+const int maxOvers = 4;	// Maximum Overs allowed to a Bowler to bowl in a single Innings
 
+
+// Stores name and a vector of Players
+// Stores all the already out Batsmen and Overs Bowled by each Bowler
+// Returns next batsman and bowler asking from the user
+// Provides functions to write/read whole Team to/from a file
 class Team
 {
-
 public:
-	Team();
-	Team(std::string name, std::vector<Player> newPlayers);
-	Team(std::string name);
-	~Team();
 
+	Team();	// Default Constructor
+	Team(std::string name);	// Parameterized Constructor to initialize name to given name and players to empty vector
+	Team(std::string name, std::vector<Player> newPlayers);	// Parametrized Constructor to initialize name and players 
+
+	~Team();	// Destructor
+
+	// Member Functions
+	void addPlayer(const Player& player);	// Add the given Player to players
+	Player* getNextBatsman(std::vector<Player>& batsmenPlayed);	// Calls displayBatsmen() and asks for batsman name from user
+																// Finds the batsman in players
+																// repeats untill gets a valid batsman who is not out
+																// Returns a reference to the player
+
+	Player* getNextBowler();	// Calls displayBowlers() and asks for bowler name from user
+								// Finds the bowler in players, repeats untill gets a valid bowler who can bowl
+								// Returns a reference to the player
+
+	void playerOut(const Player& player);	// Declares the given Player to be out, assumes player not already out(does nothing if it is)
+	bool canPlay(int nWickets, int nOvers) const;	// Checks if the Team has sufficient players to play
+													// Based on nWickets, nOvers, maxOvers
+
+	void reset();	// Clears out, sets Overs bowled by each Bowler to 0
+
+	void updateBatsmen(const std::vector<Player>& batsmenPlayed);	// Increases total Matches of all batsmen who played by 1
+	void updateBowlers();	// Increases total Overs of all Bowlers who bowled by no. of Overs Bowled
+
+	void write(std::ofstream& ofs) const;	// Adds Team name and all Players to the file Buffer in a specified format
+	void read(std::ifstream& ifs);	// Reads name of team and all Players from the File Buffer in a specified format
+
+
+	// Operator Overloads
+	friend bool operator==(const Team& team1, const Team& team2);
+	friend bool operator!=(const Team& team1, const Team& team2);
+
+
+	// Data Members
 	std::string name;
 	std::vector<Player> players;
 
 
-
-	void addPlayer(const Player& player);
-	Player* getNextBatsman(std::vector<Player>& batsmenPlayed);
-	Player* getNextBowler();
-	void reset();
-	void displayBatsmen();
-	void displayBowlers();
-	void playerOut(const Player& player);
-	bool canPlay(int nWickets, int nOvers) const;
-
-	void updateBatsmen(const std::vector<Player>& batsmenPlayed);
-	void updateBowlers();
-
-	void write(std::ofstream& ofs) const;
-	void read(std::ifstream& ifs);
-
-
-	friend bool operator==(const Team& team1, const Team& team2);
-	friend bool operator!=(const Team& team1, const Team& team2);
-
 private:
-
+	// Data Members
 	std::vector<Player> out;
 	std::unordered_map<Player, int> oversBowled;
 
-	bool canBowl(const Player& player);
+	// Member Functions
+	void displayBatsmen();	// Display all Players who can Bat next
+	void displayBowlers();	// Display all Players who can Bowl next
+	bool canBowl(const Player& player);	// Checks if a given Player is eligible to bowl or not, i.e. if he has already bowled maxOvers or not
 };
 
 
-bool operator==(const Team& team1, const Team& team2);
-bool operator!=(const Team& team1, const Team& team2);
+// Operator Overloads
+bool operator==(const Team& team1, const Team& team2);	// Equality Based on Team Name only
+bool operator!=(const Team& team1, const Team& team2);	// Inequality, acts as a negation to Equality Operator
 
 
 
+// HELPER FUNCTIONS
 template<class T>
-int findPos(std::vector<T>& elements, const T& element)
+int findPos(std::vector<T>& elements, const T& element)	// Returns the position of an element in a vector of elements
+														// Returns -1 if the element is not present.
 {
 	for (int i = 0; i < elements.size(); ++i) {
 		if (element == elements[i])
