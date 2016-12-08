6815d10f72b55e544becbe6d339b56432a98bde4
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2016-12-08 11:38:23 +0530

    Adding Comments to Game

--- Game.cpp
+++ Game.cpp
@@ -1,63 +1,258 @@
 #include "Game.h"
-#include <ctime>
-#include <random>
-#include <iostream>
-#include "Input.h"
-#include "Result.h"	// NOT REQUIRED
+
+#include <ctime>	// For time_t, time()
+#include <random>	// For rand(), srand()
+#include <iostream>	// For cout
+
+#include "Input.h"	// For getNum(), getString
+#include "Result.h"	// Just for completion of Documentation, Not required, already included in Ball.h
 #include "Ball.h"
 
 using namespace std;
 
-Game::Game(vector<Team>& teams)
+
+Game::Game(vector<Team>& teams)	// Constructor to initialize own teams with the given teams vector
 	:teams{ teams }, leftWickets{ 0 }, leftBalls{ 0 }
 {
 }
-
-
-Game::~Game()
+Game::~Game()	// Destructor
 {
 }
 
 
-void Game::init()
+// PUBLIC MEMBER FUNCTIONS
+void Game::play()
 {
-	leftWickets = nWickets;
-	leftBalls = nBalls;
+	// Check if Match is possible or not
+	if (!areTeamsEligible())
+		return;
+
+	// Start the Play if there are eligible Teams
+	system("cls");
+	cout << "***************  PLAY  ***************";
+
+	// Display all the eligible teams
+	cout << "\n\n*******************\n#      TEAMS      #\n*******************\n";
+
+	for (const Team &team : teams) {	// For each team in teams
+		// Display the team if it is eligible to play
+		if (team.canPlay(nWickets, ceil(static_cast<double>(nBalls) / ballsPerOver)))
+			cout << endl << team.name;
+	}
+	cout << "\n\n#-----------------#\n\n";
 
 
-	// get random value acc. to current system time
-	time_t currTime;
-	time(&currTime);
-	srand((unsigned int)currTime);
+	// Get position of first Playing Team
+	int posTeam1 = posPlayingTeam("\n\nEnter name of first team:\n");
+	// Get position of second Playing Team
+	int posTeam2 = posPlayingTeam("\n\nEnter name of second team:\n");
+
+	while (posTeam2 == posTeam1) {	// Ask for second Team untill user enters a valid Team different from first Team
+		cout << "\nTeam: " << teams[posTeam2].name << " already playing!! Please try again.....\n";
+		// Try to get the Team again
+		posTeam2 = posPlayingTeam("\n\nEnter name of second team:\n");
+	}
+	// We got the two teams to play
+
+	// Initialize the parameters, and seed rand()
+	init();
+
+	// TOSS
+	if (toss(teams[posTeam1].name, teams[posTeam2].name)) {
+		// If toss returned true, i.e. First Team gets the first Batting
+		match(teams[posTeam1], teams[posTeam2]);	// match with Team 1 as the first Batting Team
+	}
+	else	// If Toss returned false, i.e. First Team gets the first Bowling
+		match(teams[posTeam2], teams[posTeam1]);	//match with Team 2 as the first Batting Team
 }
 
 
-bool Game::tossChoice(const string& tName)
-{
-	cout << endl << tName << " Won the Toss!!\n\n";
+// PRIVATE MEMBER FUNCTIONS
 
+void Game::match(Team& team1, Team& team2)
+{
+	// Assumes first Team is the Team to Bat First
+
+	// INNINGS 1
+	system("cls");
+	cout << "***************  INNINGS 1:  ***************\n";
+	int runst1 = innings(team1, team2);	// Simulate the innings and get the Runs Scored
+	_sleep(1000);
+	cout << "\n#----------- END OF INNINGS 1 -----------#\n";
+	_sleep(1000);
+	cout << "\nRuns made by first team: " << runst1;
+	_sleep(3000);
+
+
+	// INNINGS 2
+	system("cls");
+	cout << "***************  INNINGS 2:  ***************\n";
+	int runst2 = innings(team2, team1);	// Simulate the innings with team2 as Batting Team now and gets the Runs scored
+	_sleep(1000);
+	cout << "\n#----------- END OF INNINGS 2 -----------#\n";
+	_sleep(1000);
+	cout << "\nRuns made by second team: " << runst2;
+	_sleep(3000);
+
+	// Display the Final Result
+	system("cls");
+	cout << "************  RESULT  ************\n";
 	_sleep(2000);
+	if (runst1 > runst2) {
+		cout << "\n" << team1.name << " Wins by " << runst1 - runst2 << " runs!!\n";
+	}
+	else if (runst1 < runst2) {
+		cout << "\n" << team2.name << " Wins by " << runst2 - runst1 << " runs!!\n";
+	}
+	else {		// runst1 == runst2, i.e. a DRAW
+		cout << "\nDRAW between " << team1.name << " and " << team2.name;
+	}
+	cout << "\n\n";
+	_sleep(2000);
+}
 
-	int input = getNum(tName + ": You want Batting or Bowling(1/2)?\n");
-	while (input != 1 && input != 2) {
-		cout << "\nSorry, wrong input!! Please try again.....\n";
-		input = getNum(tName + ": You want Batting or Bowling(1/2)?\n");
+int Game::innings(Team& battingTeam, Team& bowlingTeam)
+{
+	init();	// Initialize the Game Parameters
+
+	int totalRuns = 0;
+	int totalFours = 0;
+	int totalSixes = 0;
+	int totalWickets = 0;
+
+	vector<Player> batsmanPlayed;	// Stores all the batsmen who did Batting in the Innings
+
+	// Get the Opening Batsman and Bowler from the two teams
+	Player* batsman = battingTeam.getNextBatsman(batsmanPlayed);
+	Player* bowler = bowlingTeam.getNextBowler();
+
+	// Actual Start of Innings
+	system("cls");
+	cout << "**********  Over: 1  **********\n";
+	while (leftWickets > 0 && leftBalls > 0) {
+
+		if (leftBalls != nBalls && leftBalls%ballsPerOver == 0) {
+			// Over is ended if left Balls is a multiple of balls per over
+			_sleep(1000);
+			cout << "\nEnd of Over!!\nRuns till now: " << totalRuns << endl;
+			_sleep(2000);
+			// Get Bowler to bowl next Over
+			bowler = bowlingTeam.getNextBowler();
+			system("cls");
+			cout << "**********  Over: " << (nBalls - leftBalls) / ballsPerOver + 1 << "  **********\n";	// Start of next Over
+		}
+
+		Result result;	// Stores result of next Ball
+
+		Ball b(*batsman, *bowler);
+		result = b.getResult();	// Get result of the current Ball
+		leftBalls--;
+
+
+		cout << "\nBowl " << nBalls - leftBalls << ": ";
+		_sleep(2000);
+
+		switch (result) {
+					// Do the appropriate according to possible values of Result
+		case Result::ZERO:
+			cout << "Zero runs.\n";
+			break;
+
+		case Result::ONE:
+			cout << "Single.\n";
+			totalRuns++;
+			(*batsman).increaseRuns(1);	// Increase total Runs Scored by Batsman
+			break;
+
+		case Result::DOUBLE:
+			cout << "Double.\n";
+			totalRuns += 2;
+			(*batsman).increaseRuns(2);	// Increase total Runs Scored by Batsman
+			break;
+
+		case Result::TRIPLE:
+			cout << "Triple.\n";
+			totalRuns += 3;
+			(*batsman).increaseRuns(3);	// Increase total Runs Scored by Batsman
+			break;
+
+		case Result::FOUR:
+			cout << "Four.\n";
+			totalRuns += 4;
+			(*batsman).increaseRuns(4);	// Increase total Runs Scored by Batsman
+			totalFours++;
+			break;
+
+		// No 5 runs for this version of the Game Simulator
+		case Result::SIX:
+			cout << "Six!!\n";
+			totalRuns += 6;
+			(*batsman).increaseRuns(6);	// Increase total Runs Scored by Batsman
+			totalSixes++;
+			break;
+
+		case Result::OUT:
+			cout << "Out!!\n";
+			battingTeam.playerOut((*batsman));	// Declare the batsman to be out to the Batting Team
+			
+			leftWickets--;
+			if (leftWickets > 0)	// Get next Batsman only if Wickets are left, else, Game Over
+				batsman = battingTeam.getNextBatsman(batsmanPlayed);
+
+			(*bowler).increaseWickets(1);	// Increase total Wickets taken by Bowler
+			break;
+
+		case Result::RUNOUT:
+			cout << "Run Out!!\n";
+			battingTeam.playerOut((*batsman));	// Declare the batsman to be out to the Batting Team
+
+			leftWickets--;
+			if (leftWickets > 0)	// Get next Batsman only if Wickets are left, else, Game Over
+				batsman = battingTeam.getNextBatsman(batsmanPlayed);
+			
+			(*bowler).increaseWickets(1);	// Increase total Wickets taken by Bowler
+			break;
+
+		}
+
+
 	}
 
-	if (input == 1) {
-		cout << "\n\n" << tName << " won the toss and chose to BAT first.....";
-		_sleep(2000);
-		cout << "\n\n\nStarting the game.....";
-		_sleep(2000);
-		return true;
+	// Update the Total Matches and total Overs of Batsmen and Bowers who did Batting/ bowled Overs
+	battingTeam.updateBatsmen(batsmanPlayed);
+	bowlingTeam.updateBowlers();
+
+	// Reset the out Batsmen and the overs Bowled by the Bowlers
+	battingTeam.reset();
+	bowlingTeam.reset();
+
+	return totalRuns;
+}
+
+int Game::posPlayingTeam(const string& prompt)
+{
+	int posTeam = -1;	// Signal for not got playing team yet
+
+	while (posTeam == -1) {
+		// While we do not get a valid Playing Team
+		string teamName = getString(prompt);
+
+		posTeam = findPos(teams, Team(teamName));	// Returns -1 if Team is not found
+		if (posTeam == -1) {	// User entered Wrong Team name
+			cout << "\nTeam not found: " << teamName << "!! Please try again.....\n";
+		}
+		else if (!teams[posTeam].canPlay(nWickets, ceil(static_cast<double>(nBalls) / ballsPerOver))) {
+			// Team entered is not eligible to play
+			cout << endl << teams[posTeam].name << " has only " << teams[posTeam].players.size() << " players!!";
+
+			int bowlersReq = ceil(static_cast<double>(nBalls) / ballsPerOver / 4);
+			cout << "\nMinimum players required to play : " << (nWickets > bowlersReq ? nWickets : bowlersReq) << "\n\n";
+			posTeam = -1;	// We did not get a Playing Team yet
+		}
+
 	}
-	else {
-		cout << "\n\n" << tName << " won the toss and chose to BOWL first.....";
-		_sleep(2000);
-		cout << "\n\n\nStarting the game.....";
-		_sleep(2000);
-		return false;
-	}
+
+	return posTeam;
 }
 
 bool Game::toss(const string& tName1, const string& tName2)
@@ -66,251 +261,86 @@ bool Game::toss(const string& tName1, const string& tName2)
 	cout << "**********  TOSS  **********";
 	_sleep(2000);
 
+	int outcome = rand() % 2;	// Get 0 or 1 randomly
 
-	int outcome = rand() % 2;
-	if (outcome == 0) {
+	if (outcome == 0) {	// HEADS
 		cout << "\n\nHEADS!!\n";
 		_sleep(1000);
-		bool choice = tossChoice(tName1);
-		if (choice)
+		bool choice = tossChoice(tName1);	// Get the Choice of Team which Won the Toss
+		
+		if (choice)	// Team 1 Chose to Bat first
 			return true;
-		return false;
+		return false;	// Team 2 gets first Batting
 	}
-	else {
+	else {	// TAILS
 		cout << "\n\nTAILS!!\n";
 		_sleep(1000);
-		bool choice = tossChoice(tName2);
-		if (choice)
+		bool choice = tossChoice(tName2);	// Get the Choice of Team which Won the Toss
+		
+		if (choice)	// Team 2 chose to Bat first
 			return false;
+		return true;	// Team 1 gets the first Batting
+	}
+}
+
+bool Game::tossChoice(const string& tName)
+{
+	cout << endl << tName << " Won the Toss!!\n\n";
+	_sleep(2000);
+
+
+	int input = getNum(tName + ": You want Batting or Bowling(1/2)?\n");	// Get User's choice: a number
+	
+	while (input != 1 && input != 2) {	// User entered wrong Input
+		cout << "\nSorry, wrong input!! Please try again.....\n";
+		input = getNum(tName + ": You want Batting or Bowling(1/2)?\n");
+	}
+
+	if (input == 1) {	// Chose to Bat first
+		cout << "\n\n" << tName << " won the toss and chose to BAT first.....";
+		_sleep(2000);
+
+		cout << "\n\n\nStarting the game.....";
+		_sleep(2000);
 		return true;
 	}
-}
-
-
-int Game::innings(Team& battingTeam, Team& bowlingTeam)
-{
-	init();
-
-	int totalRuns = 0;
-	int totalFours = 0;
-	int totalSixes = 0;
-	int totalWickets = 0;
-
-	vector<Player> batsmanPlayed;
-
-	Player* batsman = battingTeam.getNextBatsman(batsmanPlayed);
-	Player* bowler = bowlingTeam.getNextBowler();
-
-	system("cls");
-	cout << "**********  Over: 1  **********\n";
-	while (leftWickets > 0 && leftBalls > 0) {
-
-		if (leftBalls != nBalls && leftBalls%ballsPerOver == 0) {
-			_sleep(1000);
-			cout << "\nEnd of Over!!\nRuns till now: " << totalRuns << endl;
-			_sleep(2000);
-			bowler = bowlingTeam.getNextBowler();
-			system("cls");
-			cout << "**********  Over: " << (nBalls - leftBalls) / ballsPerOver + 1 << "  **********\n";
-		}
-
-		Result result;
-		Ball b(*batsman, *bowler);
-		result = b.getResult();
-		leftBalls--;
-
-
-		cout << "\nBowl " << nBalls - leftBalls << ": ";
+	else {		// Chose to Bowl first
+		cout << "\n\n" << tName << " won the toss and chose to BOWL first.....";
 		_sleep(2000);
-		switch (result) {
 
-		case Result::ZERO:
-			cout << "Zero runs.\n";
-			break;
-
-		case Result::ONE:
-			cout << "Single.\n";
-			totalRuns++;
-			(*batsman).increaseRuns(1);
-			break;
-
-		case Result::DOUBLE:
-			cout << "Double.\n";
-			totalRuns += 2;
-			(*batsman).increaseRuns(2);
-			break;
-
-		case Result::TRIPLE:
-			cout << "Triple.\n";
-			totalRuns += 3;
-			(*batsman).increaseRuns(3);
-			break;
-
-		case Result::FOUR:
-			cout << "Four.\n";
-			totalRuns += 4;
-			(*batsman).increaseRuns(4);
-			totalFours++;
-			break;
-
-		case Result::SIX:
-			cout << "Six!!\n";
-			totalRuns += 6;
-			(*batsman).increaseRuns(6);
-			totalSixes++;
-			break;
-
-		case Result::OUT:
-			cout << "Out!!\n";
-			leftWickets--;
-			battingTeam.playerOut((*batsman));
-			if (leftWickets > 0)
-				batsman = battingTeam.getNextBatsman(batsmanPlayed);
-			(*bowler).increaseWickets(1);
-			break;
-
-		case Result::RUNOUT:
-			cout << "Run Out!!\n";
-			leftWickets--;
-			battingTeam.playerOut((*batsman));
-			if (leftWickets > 0)
-				batsman = battingTeam.getNextBatsman(batsmanPlayed);
-			(*bowler).increaseWickets(1);
-			break;
-
-		}
-
-
+		cout << "\n\n\nStarting the game.....";
+		_sleep(2000);
+		return false;
 	}
-
-
-	battingTeam.updateBatsmen(batsmanPlayed);
-	bowlingTeam.updateBowlers();
-
-	battingTeam.reset();
-	bowlingTeam.reset();
-
-	return totalRuns;
 }
-
-
-int Game::match(Team& team1, Team& team2)
-{
-	system("cls");
-	cout << "***************  INNINGS 1:  ***************\n";
-	int runst1 = innings(team1, team2);
-	_sleep(1000);
-	cout << "\n#----------- END OF INNINGS 1 -----------#\n";
-	_sleep(1000);
-	cout << "\nRuns made by first team: " << runst1;
-
-	_sleep(3000);
-
-	system("cls");
-	cout << "***************  INNINGS 2:  ***************\n";
-	int runst2 = innings(team2, team1);
-	_sleep(1000);
-	cout << "\n#----------- END OF INNINGS 2 -----------#\n";
-	_sleep(1000);
-	cout << "\nRuns made by second team: " << runst2;
-
-	_sleep(3000);
-
-	system("cls");
-
-	cout << "************  RESULT  ************\n";
-	_sleep(2000);
-	if (runst1 > runst2) {
-		cout << "\n" << team1.name << " Wins by " << runst1 - runst2 << " runs!!\n";
-		return 1;
-	}
-	else if (runst1 < runst2) {
-		cout << "\n" << team2.name << " Wins by " << runst2 - runst1 << " runs!!\n";
-		return -1;
-	}
-	else {
-		cout << "\nDRAW between " << team1.name << " and " << team2.name;
-		return 0;
-	}
-	cout << "\n\n";
-	_sleep(2000);
-}
-
-
-void Game::play()
-{
-	// Find eligible teams
-	if (!areTeamsEligible())
-		return;
-
-	system("cls");
-	cout << "***************  PLAY  ***************";
-	cout << "\n\n*******************\n#      TEAMS      #\n*******************\n";
-	for (const Team &team : teams) {
-		cout << endl << team.name;
-	}
-	cout << "\n\n#-----------------#\n\n";
-
-
-
-	int posTeam1 = posPlayingTeam("\n\nEnter name of first team:\n");
-
-	int posTeam2 = posPlayingTeam("\n\nEnter name of second team:\n");
-
-	while (posTeam2 == posTeam1) {
-		cout << "\nTeam: " << teams[posTeam2].name << " already playing!! Please try again.....\n";
-		posTeam2 = posPlayingTeam("\n\nEnter name of second team:\n");
-	}
-
-
-	init();
-
-	if (toss(teams[posTeam1].name, teams[posTeam2].name)) {
-		match(teams[posTeam1], teams[posTeam2]);
-	}
-	else
-		match(teams[posTeam2], teams[posTeam1]);
-}
-
 
 bool Game::areTeamsEligible()
 {
 	int eligibleTeams = 0;
 
 	for (const Team& team : teams) {
-		if (team.canPlay(nWickets, ceil(static_cast<double>(nBalls) / ballsPerOver)))
+		if (team.canPlay(nWickets, ceil(static_cast<double>(nBalls) / ballsPerOver)))	// Team is eligible
 			++eligibleTeams;
 	}
 
+	// Teams required for a match: 2
 	if (eligibleTeams < 2) {
 		cout << "\nNot enough teams to play!!\n\n";
 		return false;
 	}
 
+	// We have eligible Teams
 	return true;
 }
 
-
-int Game::posPlayingTeam(const string& prompt)
+void Game::init()
 {
-	int posTeam = -1;
+	// Reset Game Parameters
+	leftWickets = nWickets;
+	leftBalls = nBalls;
 
-	while (posTeam == -1) {
-
-		string teamName = getString(prompt);
-
-		posTeam = findPos(teams, Team(teamName));
-		if (posTeam == -1) {
-			cout << "\nTeam not found: " << teamName << "!! Please try again.....\n";
-		}
-		else if (!teams[posTeam].canPlay(nWickets, ceil(static_cast<double>(nBalls) / ballsPerOver))) {
-			cout << endl << teams[posTeam].name << " has only " << teams[posTeam].players.size() << " players!!";
-			int bowlersReq = ceil(static_cast<double>(nBalls) / ballsPerOver / 4);
-			cout << "\nMinimum players required to play : " << (nWickets > bowlersReq ? nWickets : bowlersReq) << "\n\n";
-			posTeam = -1;
-		}
-
-	}
-
-	return posTeam;
+	// Seed the Randum number generator with current system time in seconds
+	time_t currTime;
+	time(&currTime);
+	srand((unsigned int)currTime);
 }
--- Game.h
+++ Game.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <vector>	// NOT REQUIRED
-#include <string>	// NOT REQUIRED
+
+#include <vector>	// Just for completion of Documentation, Not required, already included in Team.h
+#include <string>	// Just for completion of Documentation, Not required, already included in Team.h
+
 #include "Player.h"
 #include "Team.h"
 
@@ -9,26 +11,55 @@ const int nWickets = 1;
 const int nBalls = 30;
 const int ballsPerOver = 6;
 
+// Used for playing the game among any two teams
+// Needs to be given a reference of vector of all the teams
+// Selection of Batting/Bowling Team, Toss, Evaluating both Innings
+//  only Constructor need to be called with teams, and play() to simulate the match
 class Game
 {
 public:
-	Game(std::vector<Team>& teams);
-	~Game();
 
-	void play();
+	Game(std::vector<Team>& teams);	// Constructor to initialize its own teams reference to the teams reference passed in constructor
+	~Game();	// Destructor
+
+	// MEMBER FUNCTIONS
+	void play();	// Match starts by calling this function
+					// Decides the two valid teams and simulate the match between them
+					// Only function that need to be called for simulation
+
 
 private:
+	// DATA MEMBERS
 	std::vector<Team>& teams;
 	int leftWickets;
 	int leftBalls;
 
-	void init();
-	bool tossChoice(const std::string& tName);
-	bool toss(const std::string& tName1, const std::string& tName2);
-	int innings(Team& battingTeam, Team& bowlingTeam);
-	int match(Team& team1, Team& team2);
+	// MEMBER FUNCTIONS
+	void match(Team& team1, Team& team2);	// Simulates match between the two teams given to it
+											// Calls innings() to simulate the individual innings but with reverse order of parameters
+											// Checks result solely on the Runs scored by the two teams
 
-	bool areTeamsEligible();
-	int posPlayingTeam(const std::string& prompt);
+	int innings(Team& battingTeam, Team& bowlingTeam);	// Simulates the individual innings
+														// Runs till all batsmen are out or all balls are over
+														// Uses Ball to get result, and do the required based on the Result of each Ball
+														// returns the total runs scored by the batting team
+
+	int posPlayingTeam(const std::string& prompt);	// asks for the playing team name from the user
+													// loops untill a valid team with sufficient players is received
+													// returns the position of the team in the teams vector
+
+	bool toss(const std::string& tName1, const std::string& tName2);	// Simulates Toss between the two teams randomly
+																		// Assumes first team has HEADS and second team has TAILS
+																		// Calls tossChoice() for team's choice of Batting/Bowling
+																		// Returns true if first team is Batting Team, false in reverse case\
+
+	bool tossChoice(const std::string& tName);	// Asks for the teams choice of Batting/ Bowling
+												// Returns true if team chooses to Bat first, false, if it chooses to Bowl first
+
+	bool areTeamsEligible();	// Checks whether there are sufficient teams which are eligible to play
+								// Eligibility based on Players avalable and required in an Innings for Batting and Bowling
+
+	void init();	// Initializes an Innings by setting left Balls and left Wickets equal to nBalls and nWickets
+					// Also acts as the seeder of srand(), seeds srand() by the current system time in seconds.
+
 };
-
